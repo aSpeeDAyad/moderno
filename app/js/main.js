2685d3fdd710c7fd1695f2f5fb407225795d6d85
Your NameaSpeeDAyad <dmitryiorzhev@gmail.com>
2021-04-01 08:26:04 +0300

    add new page New-Items and slick range to sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,21 @@
 $(function(){
-
+    
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+  });
+  
     var mixer = mixitup('.products__inner-box');
 
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
-        readOnly: true
+        readOnly: false
       });
 
       $(".product-slider__inner").slick({
@@ -16,5 +26,7 @@ $(function(){
         
                
       });
+      
+     
 
 }); 
