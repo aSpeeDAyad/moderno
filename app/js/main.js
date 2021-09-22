24856ced84759bf0888bb01c62d8527c0f45a665
aSpeeDAyad <dmitryiorzhev@gmail.com>
2021-09-22 17:06:56 +0300

    add normal view menu

--- app/js/main.js
+++ app/js/main.js
@@ -55,16 +55,34 @@ btns.forEach((element) =>{
         e.target.classList.toggle('svg-red');       
         e.target.classList.toggle('active-menu');
 
-    }) 
+    },false) 
 
 });
 
 
 
 
-document.querySelectorAll(".header__user").forEach((element) =>{
-    element.addEventListener('click',()=>{
-        element.classList.toggle('active-menu');
-    })
-});
+let hdrUser = document.querySelector(".header__user");
+let hdrUserLogo = document.querySelector(".header__user-logo");
+let hdrUserName = document.querySelector(".header__user-name");
+let hdrUserStatus = document.querySelector(".header__user-status");
 
+hdrUserLogo.addEventListener('click',(e)=>{
+    hdrUser.classList.toggle('active-menu');
+});
+hdrUserStatus.addEventListener('click',(e)=>{
+    hdrUser.classList.toggle('active-menu');
+});
+let basketItem = document.querySelectorAll('.basket__item-bell');
+let basketItemImg = document.querySelectorAll('.basket__item-bell-imgbell');
+
+basketItem.forEach((element) =>{
+    element.addEventListener('click',(e)=>{
+        basketItemImg.classList.toggle('active');
+    });
+});
+// basketItemImg.forEach((element) =>{
+//     element.addEventListener('click',(e)=>{
+//         element.classList.toggle('active');
+//     });
+// });
