c32d4b007dafff4394a38b743f93bbec290739ab
aSpeeDAyad <dmitryiorzhev@gmail.com>
2021-10-18 16:53:42 +0300

    start work with adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
-
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -37,10 +35,30 @@ $(function(){
         
 
     }); 
-  
-    
-}); 
+    var mixer = mixitup('.products__inner-box');
 
+}); 
+let btnHeaderMenu = document.querySelector(".menu__btn");
+let listHeaderMenu = document.querySelector(".menu__list");
+let listHeaderMenuLi = document.querySelectorAll(".menu__list li ");
+
+
+btnHeaderMenu.addEventListener('click',()=>{
+    if(listHeaderMenu.style.display=='block'){
+        listHeaderMenu.style.display='none';
+        console.log('ppp');
+    }
+    else{
+        listHeaderMenu.style.display='block';
+        listHeaderMenu.style.position='absolute';
+
+        listHeaderMenuLi.forEach((item)=>{
+            item.style='display:block';
+        }); 
+        console.log('uuu'); 
+    }
+    console.log('jjj'); 
+});
 
 
 let hdrUserMenu = document.querySelector(".header__user-menu");
@@ -48,32 +66,29 @@ let hdrUser = document.querySelector(".header__user");
 let hdrUserLogo = document.querySelector(".header__user-logo");
 let hdrUserName = document.querySelector(".header__user-name");
 let hdrUserStatus = document.querySelector(".header__user-status");
+let hdrBsktMenu = document.querySelector(".header__basket-block-menu");
 
 
 
 
 
 document.addEventListener('click',(e)=>{
-    e.stopPropagation();
+    
+    
     if(e.target.matches('.angle-right a, .header__user-menu ul, .header__user-menu, .angle-right')){
         e.preventDefault();
     }
-    else if(e.target.matches('.header__user-name')){
-        e.stopPropagation();
-        hdrUser.classList.toggle('active-menu');
-    }
     
-    else if(e.target.matches('.header__user-logo')){
-        e.stopPropagation();
+    else if(e.target.matches('.header__user-logo, .header__user-status, .header__user-name')){
+        
         hdrUser.classList.toggle('active-menu');
     }
-    else if(e.target.matches('.header__user-status')){
-        e.stopPropagation();
-        hdrUser.classList.toggle('active-menu');
-    }
-    
+   
     else{
-        hdrUser.classList.remove('active-menu');
+        if(hdrUser){
+           hdrUser.classList.remove('active-menu'); 
+        }
+        
     }
 });
 
@@ -89,30 +104,11 @@ basketItem.forEach((element) =>{
 });
 
 let btns = document.querySelectorAll(".header__btn-basket");
-let svg = document.querySelectorAll(".svg-menu");
-let svgPatch = document.querySelectorAll(".svg-menu patch");
-
-// svg.forEach((element) =>{
-//     element.addEventListener('click',(e)=>{
-//         element.parentElement.classList.toggle('active-menu'); 
-//         element.parentElement.classList.toggle('svg-red');
-//     });    
-// });
-
-
-
-// btns.forEach((element) =>{
-//     element.addEventListener('click',(e)=>{
-//         e.target.classList.toggle('svg-red');       
-//         e.target.classList.toggle('active-menu');
-//     }) 
-
-// });
 
 document.addEventListener('click',(e)=>{
-    e.stopPropagation();
+   
     let target = e.target;
-    if(target.classList.contains('header__btn-basket')){
+    if(target.matches('.header__btn-basket')){
         btns.forEach((btn) =>{
             if(target==btn){
                 if(!btn.classList.contains('active-menu','svg-red')){
@@ -125,15 +121,17 @@ document.addEventListener('click',(e)=>{
                     btn.classList.remove('active-menu','svg-red');
                 }
             }
-              
         })
-    }
+    }        
     else{
          if(!target.classList.contains('close')){
             btns.forEach((item)=>{
                 item.classList.remove('active-menu','svg-red')
             });
         }
+        
+        
+        
     } 
 });
 
